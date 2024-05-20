a1fd928a228d92e48ba28e5012040ae3309d28f1
murugappankrishna <murugappankrisna@icloud.com>
2024-05-21 00:58:32 +0530

    21052024 Add CRUD Functionality

--- JSON_CRUD/src/main/java/com/CRUDFunctionality/JSONCRUD.java
+++ JSON_CRUD/src/main/java/com/CRUDFunctionality/JSONCRUD.java
@@ -1,16 +1,19 @@
 package com.CRUDFunctionality;
 
-import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
-import com.repo.JDBCConnection;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
+import com.repo.JDBCConnection;
 
 public class JSONCRUD {
     JDBCConnection jdbcConnection = new JDBCConnection();
@@ -22,118 +25,65 @@ public class JSONCRUD {
         String insertQuery = "INSERT INTO students (student_data) VALUES (?)";
         System.out.println("enter the JSON data");
         String JSONData = ip.nextLine();
-        try {
-            if (connection != null) {
-                PreparedStatement insertJSONStatement = connection.prepareStatement(insertQuery);
-                insertJSONStatement.setString(1, JSONData);
-                int result = insertJSONStatement.executeUpdate();
-                if (result == 1) {
-                    System.out.println("Data Inserted SuccessfullyY!");
+        if (JSONData != null) {
+            try {
+                if (connection != null) {
+                    PreparedStatement insertJSONStatement = connection.prepareStatement(insertQuery);
+                    insertJSONStatement.setString(1, JSONData);
+                    int result = insertJSONStatement.executeUpdate();
+                    if (result == 1) {
+                        System.out.println("Data Inserted SuccessfullyY!");
+                    }
+
+                } else {
+                    System.out.println("Connection Failed With the DataBase");
                 }
-
-            } else {
-                System.out.println("Connection Failed With the DataBase");
+            } catch (MysqlDataTruncation e) {
+                System.out.println("Check the data Format of the Input JSON Data ");
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.out.println("Exception related to SQL");
+                e.printStackTrace();
+            } catch (JSONException e) {
+                System.out.println("Exception related to JSON");
+                e.printStackTrace();
             }
-        } catch (MysqlDataTruncation e) {
-            System.out.println("Check the data Format of the Input JSON Data ");
-            e.printStackTrace();
-        } catch (SQLException e) {
-
-            e.printStackTrace();
         }
     }
 
     public void readJSONData() {
 
-        String selectQuery = "SELECT student_data FROM students WHERE student_id=?";
-        int choice;
-        JSONObject jsonObject = null;
+        System.out.println("Enter the Student ID Whose Details are to be Retrieved");
+        int studentID = ip.nextInt();
+        JSONObject jsonObject = fetchJSONData(studentID);
+        if (jsonObject != null) {
+            try {
 
-        try {
-            if (connection != null) {
-                System.out.println("Enter the Student ID Whose Details are to be Retrieved");
-                int studentID = ip.nextInt();
-                PreparedStatement selectJSONStatement = connection.prepareStatement(selectQuery);
-                selectJSONStatement.setInt(1, studentID);
-                ResultSet rs = selectJSONStatement.executeQuery();
-                if (!rs.isBeforeFirst()) {
-                    System.out.println("No Data Found for the Student ID " + studentID + "Enter a Valid Student ID.");
+                int choice;
+                System.out
+                        .println("if you want the whole JSON data press 1 /n if you want a specific key value press 2");
+                choice = ip.nextInt();
+                if (choice == 1) {
+
+                    System.out.println(jsonObject);
+                } else if (choice == 2) {
+                    System.out.println("entere the desired key");
+                    String Key = ip.next();
+                    parseJSONObject(jsonObject, Key);
+
                 } else {
-                    while (rs.next()) {
-                        String studentData = rs.getString("student_data");
-                        jsonObject = covertStringToJSON(studentData);
-
-                    }
-                    System.out.println(
-                            "Do You Want To Get Any Data Based on the Key \nIf it is a Concrete Value Enter 1 \nIf is is a Array Press 2 \nIf it  is a JSON Object Press 3 \nIf you Want the Whole Data Press Any Key ");
-                    choice = ip.nextInt();
-                    switch (choice) {
-                        case 1:
-
-                            System.out.println("Enter the key for the concrete value:");
-                            String concreteKey = ip.next();
-                            try {
-                                if (jsonObject != null && jsonObject.has(concreteKey))
-                                    System.out.println("Value: " + jsonObject.getString(concreteKey));
-                                else
-                                    System.out.println("Enter a valid Key");
-                            } catch (JSONException e) {
-                                System.out.println("The value Corresponding to Key '" + concreteKey
-                                        + "' is Not a Concrete JSON String Value.");
-                            }
-                            break;
-                        case 2:
-                            System.out.println("Enter the Key for the Array:");
-                            String arrayKey = ip.next();
-                            try {
-                                if (jsonObject != null && jsonObject.has(arrayKey))
-                                    System.out.println("JSON Array: " + jsonObject.getJSONArray(arrayKey));
-                                else
-                                    System.out.println("Enter a Valid Key");
-                            } catch (JSONException e) {
-                                System.out
-                                        .println("The Value Corresponding to Key '" + arrayKey + "' is Not a JSON Array.");
-                            }
-                            break;
-                        case 3:
-
-                            System.out.println("Enter the Key for the JSON Object:");
-                            String objectKey = ip.next();
-                            try {
-                                if (jsonObject != null && jsonObject.has(objectKey)) {
-                                    System.out.println("JSON Nested Object: " + jsonObject.getJSONObject(objectKey));
-                                } else {
-                                    System.out.println(" Enter a Valid Key");
-                                }
-                            } catch (JSONException e) {
-                                System.out.println(
-                                        "The Value Corresponding To Key '" + objectKey + "' Is Not a JSON Object.");
-                            }
-
-                            break;
-                        default:
-                            System.out.println("Whole JSON: " + jsonObject);
-                            break;
-                    }
-
+                    System.out.println("enter either 1 or 2");
                 }
-            } else {
-                System.out.println("Connection Failed With the DataBase");
+
+            } catch (InputMismatchException e) {
+                System.out.println("enter a valid studentID of type integer");
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Enter a valid Number of type Integer");
-            e.printStackTrace();
-        } catch (SQLException e) {
-
-            e.printStackTrace();
         }
-
     }
 
     public void deleteJSONData() {
         String deleteQuery = "DELETE FROM students WHERE  student_id=?";
         System.out.println("Enter the Student ID Whose Details are to be Deleted");
-
 
         try {
             int studentID = ip.nextInt();
@@ -156,67 +106,198 @@ public class JSONCRUD {
     }
 
     public void updateJSONData() {
+        int choice;
+        System.out.println("Enter the student ID to be modified");
+        int StudentID = ip.nextInt();
+        String modifyKeyValue = null;
+        JSONObject jsonData = fetchJSONData(StudentID);
+        if (jsonData != null) {
+
+            System.out.println(
+                    "To delete a  key pair value press 1/n To update a data press 2(if exist the values gets updated else a new pair value will be created)");
+
+            choice = ip.nextInt();
+            switch (choice) {
+                case 1: {
+                    System.out.println("Enter the key value to be deleted");
+                    modifyKeyValue = ip.next();
+                    JSONObject updatedJSON = deleteKeyPairValue(jsonData, modifyKeyValue);
+                    updateQuery(updatedJSON, StudentID);
+                    break;
+
+                }
+                case 2: {
+                    System.out.println("Enter the key value to be deleted");
+                    modifyKeyValue = ip.next();
+                    System.out.println("Enter the new value to be updated");
+                    String newValue = ip.next();
+                    JSONObject updatedJSON = updateValue(jsonData, modifyKeyValue, newValue);
+
+                    updateQuery(updatedJSON, StudentID);
+                    break;
+                }
+            }
+
+        }
+    }
+
+    JSONObject covertStringToJSON(String JSONString) {
+
+        return new JSONObject(JSONString);
+
+    }
+
+    void parseJSONObject(JSONObject json, String key) {
+        boolean exists = json.has(key);
+        if (exists) {
+            System.out.println(json.get(key));
+        } else {
+            Iterator<?> keys = json.keys();
+            while (keys.hasNext()) {
+                String nextKey = (String) keys.next();
+                try {
+                    if (json.get(nextKey) instanceof JSONObject) {
+                        parseJSONObject(json.getJSONObject(nextKey), key);
+
+                    } else if (json.get(nextKey) instanceof JSONArray) {
+                        JSONArray jsonArray = json.getJSONArray(nextKey);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            if (jsonArray.get(i) instanceof JSONObject) {
+                                parseJSONObject(jsonArray.getJSONObject(i), key);
+
+                            } else {
+                                System.out.println(jsonArray.get(i));
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    // Handle JSON exception, if needed
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    JSONObject deleteKeyPairValue(JSONObject json, String key) {
+        boolean exists = json.has(key);
+        if (exists) {
+            json.remove(key);
+        } else {
+            Iterator<String> keys = json.keys();
+            while (keys.hasNext()) {
+                String nextKey = (String) keys.next();
+                try {
+                    Object value = json.get(nextKey);
+                    if (value instanceof JSONObject) {
+                        deleteKeyPairValue((JSONObject) value, key);
+                    } else if (value instanceof JSONArray) {
+                        JSONArray jsonArray = (JSONArray) value;
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            Object arrayItem = jsonArray.get(i);
+                            if (arrayItem instanceof JSONObject) {
+                                deleteKeyPairValue((JSONObject) arrayItem, key);
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return json;
+    }
+
+    JSONObject fetchJSONData(int studentID) {
         String fetchQuery = "SELECT student_data FROM students WHERE student_id=?";
         JSONObject jsonObject = null;
-        int choise;
+        if (connection != null) {
 
-        try {
-            if (connection != null) {
-                System.out.println("Enter the Student ID Whose Details  are to be Modified");
-                int studentID = ip.nextInt();
-                PreparedStatement selectJSONStatement = connection.prepareStatement(fetchQuery);
+            PreparedStatement selectJSONStatement;
+            try {
+                selectJSONStatement = connection.prepareStatement(fetchQuery);
                 selectJSONStatement.setInt(1, studentID);
                 ResultSet rs = selectJSONStatement.executeQuery();
                 if (!rs.isBeforeFirst()) {
                     System.out.println("No Data Found for the Student ID " + studentID + "Enter a Valid Student ID.");
+                    return null;
                 } else {
                     while (rs.next()) {
                         String studentData = rs.getString("student_data");
                         jsonObject = covertStringToJSON(studentData);
 
                     }
-                    System.out.println("To detele a entire key pair value press 1\n To update a data press 2(if exist the values gets updated else a new pair value will be created)");
-                    choise = ip.nextInt();
-                    switch (choise) {
-                        case 1: {
-                            System.out.println("Enter the key value to be deleted");
-                            String deleteKeyValue = ip.next();
-                            if (jsonObject != null && jsonObject.has(deleteKeyValue)) {
-                                jsonObject.remove(deleteKeyValue);
-                                String updatedStudentData = jsonObject.toString();
-                                String updateQuery = "UPDATE students SET student_data = ? WHERE student_id = ?";
-                                PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+                }
+            } catch (SQLException e) {
 
+                e.printStackTrace();
+            }
 
-                                updateStatement.setString(1, updatedStudentData);
-                                updateStatement.setInt(2, studentID);
+        }
+        return jsonObject;
+    }
 
-
-                                int rowsAffected = updateStatement.executeUpdate();
-
-
-                                if (rowsAffected == 1) {
-                                    System.out.println("Student data updated successfully.");
-                                } else {
-                                    System.out.println("Failed to update student data.");
-                                }
-                            } else {
-                                System.out.println("Enter a Valid Key");
+    JSONObject updateValue(JSONObject json, String key, String newValue) {
+        boolean exists = json.has(key);
+        if (exists) {
+            json.put(key, newValue); // Update the value associated with the key
+        } else {
+            Iterator<?> keys = json.keys();
+            while (keys.hasNext()) {
+                String nextKey = (String) keys.next();
+                try {
+                    if (json.get(nextKey) instanceof JSONObject) {
+                        updateValue(json.getJSONObject(nextKey), key, newValue);
+                    } else if (json.get(nextKey) instanceof JSONArray) {
+                        JSONArray jsonArray = json.getJSONArray(nextKey);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            if (jsonArray.get(i) instanceof JSONObject) {
+                                updateValue(jsonArray.getJSONObject(i), key, newValue);
                             }
-
-
                         }
                     }
-
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
+
             }
+            if (!exists) {
+                json.put(key, newValue);
+            }
+        }
+        return json;
+    }
+
+    void updateQuery(JSONObject updatedJSON, int id) {
+        String updatedStudentData = updatedJSON.toString();
+        String updateQuery = "UPDATE students SET student_data = ? WHERE student_id = ?";
+        PreparedStatement updateStatement;
+        try {
+            updateStatement = connection.prepareStatement(updateQuery);
+            updateStatement.setString(1, updatedStudentData);
+            updateStatement.setInt(2, id);
+
+            int rowsAffected = updateStatement.executeUpdate();
+            if (rowsAffected == 1) {
+                System.out.println("Student data updated successfully.");
+            } else {
+                System.out.println("Failed to update student data.");
+            }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+
+    }
+
+    public void exit() {
+        try {
+            connection.close();
+            ip.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
-
-    JSONObject covertStringToJSON(String JSONString) {
-        return new JSONObject(JSONString);
-    }
 }
--- JSON_CRUD/src/main/java/com/main/App.java
+++ JSON_CRUD/src/main/java/com/main/App.java
@@ -1,5 +1,7 @@
 package com.main;
 
+import java.util.Scanner;
+
 import com.CRUDFunctionality.JSONCRUD;
 
 public class App {
@@ -7,12 +9,32 @@ public class App {
 	public static void main(String[] args)
 
 	{
+		int choise;
+		Scanner ip = new Scanner(System.in);
+		JSONCRUD jsonCrud = new JSONCRUD();
+		while (true) {
+			System.out.println(
+					" 1 to create a JSON Data\n 2 to read JSON Data\n 3 to delete JSON Data\n 4 to update JSON Data\n 5 To Exit");
+			choise = ip.nextInt();
+			switch (choise) {
+				case 1:
+					jsonCrud.createJSONData();
+					break;
+				case 2:
+					jsonCrud.readJSONData();
+					break;
+				case 3:
+					jsonCrud.deleteJSONData();
+					break;
+				case 4:
+					jsonCrud.updateJSONData();
+					break;
+				case 5:
+					jsonCrud.exit();
+				default:
+					ip.close();
+			}
 
-			JSONCRUD jsonCrud = new JSONCRUD();
-			// jsonCrud.createJSONData();
-			//jsonCrud.readJSONData();
-			// jsonCrud.deleteJSONData();
-			//jsonCrud.updateJSONData();
-
+		}
 	}
 }
